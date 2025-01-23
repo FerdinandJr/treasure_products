34347c968f7bcc7a104b13a1de8616b03aef0d7c
FerdinandJr <145202573+FerdinandJr@users.noreply.github.com>
2025-01-24 02:14:04 +0800

    add delete previous file configuration

--- php.sh
+++ php.sh
@@ -53,7 +53,7 @@ sudo apt install -y php8.3 php8.3-fpm php8.3-mysql
 # Configure MySQL
 echo "Configuring MySQL..."
 sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Mystore123!'; FLUSH PRIVILEGES;"
-sudo mysql -u root -pMystore123! -e "CREATE DATABASE IF NOT EXISTS my_store;"
+sudo mysql -u root -p Mystore123! -e "CREATE DATABASE IF NOT EXISTS my_store;"
 
 # Import database
 echo "Importing database..."

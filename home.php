c24c245f4bbf80159e4f0e2354ba62ae4806b296
FerdinandJr <145202573+FerdinandJr@users.noreply.github.com>
2025-01-24 02:03:30 +0800

    input year

--- home.php
+++ home.php
@@ -133,7 +133,7 @@ view_details();
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Ferdinand 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Ferdinand 2023 - 2024</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
